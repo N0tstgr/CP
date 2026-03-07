fd8342cb435783d67665d5fbf24d403c93c389c6
Notstranger <vikashkasaudhan321@gmail.com>
2026-03-07 11:15:01 +0530

    Add files via upload

---
+++ function.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printHello(){
+	cout<<"hello";
+}
+int sum(int a, int b){
+	int result = a + b;
+	return result;
+
+
+}
+int main(){
+// printHello();
+int j = sum(4,6);
+cout<<j;
+
+
+}
